539a341c19f561d7088eec02acf4138f1a475a79
Cody Salyi <Cody.Salyi@hyland.com>
2020-11-05 16:17:44 -0500

    Initial commit of Infra.Data project, update mvc for new UniversalDbContext

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversalDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversalDbContext : DbContext
+    {
+        public UniversalDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Course { get; set; }
+    }
+}
